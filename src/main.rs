325c0e8b5bb5bc2973082e40aa4f424e099953be
kahboom <rachelyordan@gmail.com>
2021-10-17 20:45:03 +0100

    add index route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+#[macro_use] extern crate rocket;
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
